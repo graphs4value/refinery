eec2d3b70442999a879aa673e52c1144eec4e351
Kristóf Marussy <marussy@mit.bme.hu>
2021-10-01 15:52:11 +0200

    feat: skeleton for language to store mapping

---
+++ language-to-store/src/main/java/org/eclipse/viatra/solver/language/mapping/PartialModelMapper.java
@@ -0,0 +1,11 @@
+package org.eclipse.viatra.solver.language.mapping;
+
+import org.eclipse.viatra.solver.data.model.Model;
+import org.eclipse.viatra.solver.language.model.problem.Problem;
+
+public class PartialModelMapper {
+	public Model transformProblem(Problem problem) {
+		// TODO @Marci Implement this
+		throw new UnsupportedOperationException();
+	}
+}
