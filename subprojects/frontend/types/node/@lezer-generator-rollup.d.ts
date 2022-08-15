acbd1e9373990568a721050ee7eaa2e9981559ba
Kristóf Marussy <kristof@marussy.com>
2022-08-15 02:12:11 +0200

    chore(deps): bump frontend dependencies

--- subprojects/frontend/types/node/@lezer-generator-rollup.d.ts
+++ subprojects/frontend/types/node/@lezer-generator-rollup.d.ts
@@ -1,7 +1,7 @@
 // We have to explicitly redeclare the type of the `./rollup` ESM export of `@lezer/generator`,
 // because TypeScript can't find it on its own even with `"moduleResolution": "Node16"`.
 declare module '@lezer/generator/rollup' {
-  import type { Plugin } from 'rollup';
+  import type { PluginOptions } from 'vite';
 
-  export function lezer(): Plugin;
+  export function lezer(): PluginOptions;
 }
