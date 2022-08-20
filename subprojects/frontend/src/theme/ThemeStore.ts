567c66ff87833fcc5404e7bb65d736c5796b5861
Kristóf Marussy <kristof@marussy.com>
2022-08-21 00:32:39 +0200

    feat(frontend): try to match OS theme

--- subprojects/frontend/src/theme/ThemeStore.ts
+++ subprojects/frontend/src/theme/ThemeStore.ts
@@ -1,16 +1,50 @@
-import { action, makeObservable, observable } from 'mobx';
+import { action, computed, makeObservable, observable } from 'mobx';
+
+export enum ThemePreference {
+  System,
+  PreferLight,
+  PreferDark,
+}
 
 export default class ThemeStore {
-  darkMode = true;
+  preference = ThemePreference.System;
+
+  systemDarkMode: boolean;
 
   constructor() {
+    const mediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
+    this.systemDarkMode = mediaQuery.matches;
+    mediaQuery.addEventListener('change', (event) => {
+      this.systemDarkMode = event.matches;
+    });
     makeObservable(this, {
-      darkMode: observable,
+      preference: observable,
+      systemDarkMode: observable,
+      darkMode: computed,
       toggleDarkMode: action,
     });
   }
 
+  get darkMode(): boolean {
+    switch (this.preference) {
+      case ThemePreference.PreferLight:
+        return false;
+      case ThemePreference.PreferDark:
+        return true;
+      default:
+        return this.systemDarkMode;
+    }
+  }
+
   toggleDarkMode(): void {
-    this.darkMode = !this.darkMode;
+    if (this.darkMode) {
+      this.preference = this.systemDarkMode
+        ? ThemePreference.PreferLight
+        : ThemePreference.System;
+    } else {
+      this.preference = this.systemDarkMode
+        ? ThemePreference.System
+        : ThemePreference.PreferDark;
+    }
   }
 }
