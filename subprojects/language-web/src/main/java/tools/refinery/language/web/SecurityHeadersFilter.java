681439bfbf3311efeb05f8732f4742cd180d3941
Kristóf Marussy <kristof@marussy.com>
2022-10-08 20:51:20 +0200

    refactor(frontend): tighten security headers

--- subprojects/language-web/src/main/java/tools/refinery/language/web/SecurityHeadersFilter.java
+++ subprojects/language-web/src/main/java/tools/refinery/language/web/SecurityHeadersFilter.java
@@ -10,14 +10,17 @@ public class SecurityHeadersFilter implements Filter {
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException,
 			ServletException {
 		if (response instanceof HttpServletResponse httpResponse) {
-			httpResponse.setHeader("Content-Security-Policy", "default-src 'self'; " +
+			httpResponse.setHeader("Content-Security-Policy", "default-src 'none'; " +
+					"script-src 'self'; " +
 					// CodeMirror needs inline styles, see e.g.,
 					// https://discuss.codemirror.net/t/inline-styles-and-content-security-policy/1311/2
 					"style-src 'self' 'unsafe-inline'; " +
 					// Use 'data:' for displaying inline SVG backgrounds.
 					"img-src 'self' data:; " +
-					"object-src 'none'; " +
-					"base-uri 'none';");
+					"font-src 'self'; " +
+					"connect-src 'self'; " +
+					"manifest-src 'self'; " +
+					"worker-src 'self';");
 			httpResponse.setHeader("X-Content-Type-Options", "nosniff");
 			httpResponse.setHeader("X-Frame-Options", "DENY");
 			httpResponse.setHeader("Referrer-Policy", "strict-origin");
