5eab6e9e299f84da9898167568d76394657dee70
Kristóf Marussy <kristof@marussy.com>
2023-02-03 14:11:27 +0100

    refactor: Symbol as record
    Makes the intent of the code clearer by explicitly overriding equals and
    hashCode for by-reference comparison. Also fixes Sonar warning.

--- subprojects/store/src/main/java/tools/refinery/store/representation/Symbol.java
+++ subprojects/store/src/main/java/tools/refinery/store/representation/Symbol.java
@@ -2,40 +2,20 @@ package tools.refinery.store.representation;
 
 import java.util.Objects;
 
-public final class Symbol<T> implements AnySymbol {
-	private final String name;
-	private final int arity;
-	private final Class<T> valueType;
-	private final T defaultValue;
-
-	public Symbol(String name, int arity, Class<T> valueType, T defaultValue) {
-		this.name = name;
-		this.arity = arity;
-		this.valueType = valueType;
-		this.defaultValue = defaultValue;
-	}
-
-	@Override
-	public String name() {
-		return name;
-	}
-
-	@Override
-	public int arity() {
-		return arity;
-	}
-
-	@Override
-	public Class<T> valueType() {
-		return valueType;
-	}
-
-	public T defaultValue() {
-		return defaultValue;
-	}
-
+public record Symbol<T>(String name, int arity, Class<T> valueType, T defaultValue) implements AnySymbol {
 	public boolean isDefaultValue(T value) {
 		return Objects.equals(defaultValue, value);
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		return this == o;
+	}
+
+	@Override
+	public int hashCode() {
+		// Compare by identity to make hash table lookups more efficient.
+		return System.identityHashCode(this);
 	}
 
 	@Override
