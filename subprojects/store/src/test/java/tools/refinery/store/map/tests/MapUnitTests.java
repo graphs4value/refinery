f0f04091e31f80f253f9129ca3a7191d0af6af1e
OszkarSemerath <semerath@mit.bme.hu>
2023-02-16 00:41:09 +0100

    Additional tests for delta restoration

--- subprojects/store/src/test/java/tools/refinery/store/map/tests/MapUnitTests.java
+++ subprojects/store/src/test/java/tools/refinery/store/map/tests/MapUnitTests.java
@@ -2,6 +2,7 @@ package tools.refinery.store.map.tests;
 
 import org.junit.jupiter.api.Test;
 import tools.refinery.store.map.VersionedMapStore;
+import tools.refinery.store.map.VersionedMapStoreBuilder;
 import tools.refinery.store.map.VersionedMapStoreImpl;
 import tools.refinery.store.model.TupleHashProvider;
 import tools.refinery.store.tuple.Tuple;
@@ -17,5 +18,76 @@ class MapUnitTests {
 		assertEquals(false, out1);
 		var out2 = map.put(Tuple.of(1), true);
 		assertEquals(false, out2);
+	}
+
+	@Test
+	void deltaRestoreTest() {
+		VersionedMapStore<Integer,String> store =
+				VersionedMapStoreBuilder.<Integer,String>builder().setDefaultValue("x").buildOne();
+		var map = store.createMap();
+		map.put(1,"val");
+		var version1 = map.commit();
+		map.put(1,"x");
+		map.restore(version1);
+		System.out.println(map.getSize());
+		assertEquals(1,map.getSize());
+	}
+
+	@Test
+	void deltaRestoreTest2() {
+		VersionedMapStore<Integer,String> store =
+				VersionedMapStoreBuilder.<Integer,String>builder().setDefaultValue("x").buildOne();
+		var map = store.createMap();
+		map.put(1,"x");
+		var version1 = map.commit();
+		map.put(1,"1");
+		map.restore(version1);
+		System.out.println(map.getSize());
+		assertEquals(0,map.getSize());
+	}
+	@Test
+	void deltaRestoreTest3() {
+		VersionedMapStore<Integer,String> store =
+				VersionedMapStoreBuilder.<Integer,String>builder().setDefaultValue("x").buildOne();
+		var map = store.createMap();
+		map.commit();
+		map.put(1,"1");
+		map.put(2,"x");
+		assertEquals(1,map.getSize());
+		var version1 = map.commit();
+		map.put(1,"x");
+		assertEquals(0,map.getSize());
+		map.put(2,"2");
+		assertEquals(1,map.getSize());
+		map.put(2,"x");
+		assertEquals(0,map.getSize());
+		var version2 = map.commit();
+		map.restore(version1);
+		assertEquals(1,map.getSize());
+		map.restore(version2);
+		assertEquals(0,map.getSize());
+	}
+
+	@Test
+	void deltaRestoreTest4() {
+		VersionedMapStore<Integer,String> store =
+				VersionedMapStoreBuilder.<Integer,String>builder().setDefaultValue("x").buildOne();
+		var map = store.createMap();
+		map.commit();
+		map.put(1,"1");
+		map.put(2,"x");
+		assertEquals(1,map.getSize());
+		var version1 = map.commit();
+		map.put(1,"x");
+		assertEquals(0,map.getSize());
+		map.put(2,"2");
+		assertEquals(1,map.getSize());
+		map.put(2,"x");
+		assertEquals(0,map.getSize());
+		var version2 = map.commit();
+		map.restore(version1);
+		assertEquals(1,map.getSize());
+		map.restore(version2);
+		assertEquals(0,map.getSize());
 	}
 }
