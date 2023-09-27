d6d2a87d58792f9328a682cffa4a65a4eb4a1a8b
Kristóf Marussy <kristof@marussy.com>
2023-09-27 02:29:51 +0200

    fix: Docker image tags
    Make sure to push to both the latest tag and the current version.
    See https://stackoverflow.com/a/60724547
    
    Also fixes the tag for the Temurin JDK17 image.

--- docker/build.sh
+++ docker/build.sh
@@ -33,7 +33,7 @@ rm -rf dist
 
 docker buildx build . \
     --platform linux/amd64,linux/arm64 \
-    --output "type=image,name=ghcr.io/graphs4value/refinery:${refinery_version},name=ghcr.io/graphs4value/refinery:latest,push=true,annotation-index.org.opencontainers.image.source=https://github.com/graphs4value/refinery,annotation-index.org.opencontainers.image.description=Refinery: an efficient graph solver for generating well-formed models,annotation-index.org.opencontainers.image.licenses=EPL-2.0" \
+    --output "type=image,\"name=ghcr.io/graphs4value/refinery:${refinery_version},ghcr.io/graphs4value/refinery:latest\",push=true,annotation-index.org.opencontainers.image.source=https://github.com/graphs4value/refinery,annotation-index.org.opencontainers.image.description=Refinery: an efficient graph solver for generating well-formed models,annotation-index.org.opencontainers.image.licenses=EPL-2.0" \
     --label 'org.opencontainers.image.source=https://github.com/graphs4value/refinery' \
     --label 'org.opencontainers.image.description=Refinery: an efficient graph solver for generating well-formed models' \
     --label 'org.opencontainers.image.licenses=EPL-2.0'
