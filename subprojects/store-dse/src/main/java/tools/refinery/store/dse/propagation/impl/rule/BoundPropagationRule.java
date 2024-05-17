3effda4eb19d4e5f721af1a8db5f7c2c85763ad0
Kristóf Marussy <kristof@marussy.com>
2024-05-18 01:32:37 +0200

    feat(dse): detect stuch propagation rules

--- subprojects/store-dse/src/main/java/tools/refinery/store/dse/propagation/impl/rule/BoundPropagationRule.java
+++ subprojects/store-dse/src/main/java/tools/refinery/store/dse/propagation/impl/rule/BoundPropagationRule.java
@@ -9,31 +9,54 @@ import tools.refinery.store.dse.propagation.PropagationResult;
 import tools.refinery.store.dse.transition.Rule;
 import tools.refinery.store.dse.transition.actions.BoundAction;
 import tools.refinery.store.model.Model;
+import tools.refinery.store.model.ModelListener;
 import tools.refinery.store.query.ModelQueryAdapter;
 import tools.refinery.store.query.resultset.ResultSet;
+import tools.refinery.store.tuple.Tuple;
+
+import java.util.HashSet;
 
 class BoundPropagationRule {
 	private final Model model;
+	private final Rule rule;
 	private final ResultSet<Boolean> resultSet;
 	private final BoundAction action;
+	private final HashSet<Tuple> firedActivations = new HashSet<>();
 
 	public BoundPropagationRule(Model model, Rule rule) {
 		this.model = model;
+		this.rule = rule;
 		resultSet = model.getAdapter(ModelQueryAdapter.class).getResultSet(rule.getPrecondition());
 		action = rule.createAction(model);
+		model.addListener(new ModelListener() {
+			@Override
+			public void afterRestore() {
+				firedActivations.clear();
+			}
+		});
+		resultSet.addListener((key, fromValue, toValue) -> {
+			if (Boolean.FALSE.equals(toValue)) {
+				firedActivations.remove(key);
+			}
+		});
 	}
 
 	public PropagationResult fireAll() {
+		if (!firedActivations.isEmpty()) {
+			throw new IllegalStateException("Stuck propagation rule '%s'.".formatted(rule.getName()));
+		}
 		if (resultSet.size() == 0) {
 			return PropagationResult.UNCHANGED;
 		}
 		var cursor = resultSet.getAll();
 		while (cursor.move()) {
 			model.checkCancelled();
-			var result = action.fire(cursor.getKey());
+			var tuple = cursor.getKey();
+			var result = action.fire(tuple);
 			if (!result) {
 				return PropagationResult.REJECTED;
 			}
+			firedActivations.add(tuple);
 		}
 		return PropagationResult.PROPAGATED;
 	}
