109ae420671a72b2f3b3d905c8fc32b7ad423387
Kristóf Marussy <marussy@mit.bme.hu>
2024-10-12 20:33:31 +0200

    build: convenience wrapper task for generator-cli

--- subprojects/generator-cli/build.gradle.kts
+++ subprojects/generator-cli/build.gradle.kts
@@ -17,3 +17,16 @@ dependencies {
 application {
 	mainClass.set("tools.refinery.generator.cli.RefineryCli")
 }
+
+tasks.register<JavaExec>("cli") {
+	val mainRuntimeClasspath = sourceSets.main.map { it.runtimeClasspath }
+	dependsOn(mainRuntimeClasspath)
+	classpath(mainRuntimeClasspath)
+	mainClass.set(application.mainClass)
+	// Workaround for https://github.com/gradle/gradle/issues/6074 when running this task from a subdirectory.
+	val pwd = System.getProperty("user.dir")
+	workingDir = if (pwd == null) rootDir else file(pwd)
+	standardInput = System.`in`
+	group = "Run"
+	description = "Run the Refinery CLI application"
+}
