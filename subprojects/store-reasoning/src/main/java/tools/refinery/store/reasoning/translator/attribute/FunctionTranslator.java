a9c49357ffbfa33eb9f27783bf20ed26ee7a3e3c
Kristóf Marussy <marussy@mit.bme.hu>
2025-10-23 22:00:37 +0200

    fix(reasoning): default values for non-mutable partial functions

--- subprojects/store-reasoning/src/main/java/tools/refinery/store/reasoning/translator/attribute/FunctionTranslator.java
+++ subprojects/store-reasoning/src/main/java/tools/refinery/store/reasoning/translator/attribute/FunctionTranslator.java
@@ -9,7 +9,9 @@ import tools.refinery.logic.AbstractValue;
 import tools.refinery.logic.dnf.FunctionalQuery;
 import tools.refinery.logic.dnf.Query;
 import tools.refinery.logic.literal.CallPolarity;
+import tools.refinery.logic.term.ConstantTerm;
 import tools.refinery.logic.term.NodeVariable;
+import tools.refinery.logic.term.Term;
 import tools.refinery.logic.term.Variable;
 import tools.refinery.logic.term.abstractdomain.AbstractDomainTerms;
 import tools.refinery.store.model.ModelStoreBuilder;
@@ -23,6 +25,7 @@ import tools.refinery.store.reasoning.translator.PartialFunctionTranslator;
 import tools.refinery.store.representation.Symbol;
 
 import java.util.ArrayList;
+import java.util.function.Function;
 
 public class FunctionTranslator<A extends AbstractValue<A, C>, C> implements ModelStoreConfiguration {
 	private final PartialFunction<A, C> partialFunction;
@@ -44,36 +47,47 @@ public class FunctionTranslator<A extends AbstractValue<A, C>, C> implements Mod
 	public void apply(ModelStoreBuilder storeBuilder) {
 		var translator = PartialFunctionTranslator.of(partialFunction)
 				.domain(domainRelation);
+		var abstractDomain = partialFunction.abstractDomain();
 		if (mutable) {
 			var name = partialFunction.name();
-			var abstractDomain = partialFunction.abstractDomain();
 			var type = abstractDomain.abstractType();
-			int arity = partialFunction.arity();
 			var storageSymbol = Symbol.of(name, partialFunction.arity(), type, defaultValue);
 			translator.symbol(storageSymbol);
-			var parameters = new NodeVariable[arity];
-			var arguments = new ArrayList<Variable>(arity + 1);
-			for (int i = 0; i < arity; i++) {
-				var parameter = Variable.of("p" + i);
-				parameters[i] = parameter;
-				arguments.add(parameter);
-			}
-			var computedValue = Variable.of("computedValue", type);
-			arguments.add(computedValue);
-			var output = Variable.of("output", type);
-			var mergedQuery = Query.builder(name)
-					.parameters(parameters)
-					.output(output)
-					.clause(
-							ModalConstraint.of(Modality.MAY, query.getDnf()).call(CallPolarity.POSITIVE, arguments),
-							output.assign(AbstractDomainTerms.meet(abstractDomain, computedValue,
-									new FunctionView<>(storageSymbol).leftJoin(defaultValue, parameters)))
-					)
-					.build();
+			var mergedQuery = mergeQuery((parameters) -> new FunctionView<>(storageSymbol).leftJoin(defaultValue,
+					parameters));
 			translator.query(mergedQuery);
-		} else {
+		} else if (abstractDomain.unknown().equals(defaultValue)) {
 			translator.query(query);
+		} else {
+			var mergedQuery = mergeQuery((ignored) -> new ConstantTerm<>(abstractDomain.abstractType(), defaultValue));
+			translator.query(mergedQuery);
 		}
 		storeBuilder.with(translator);
+	}
+
+	private FunctionalQuery<A> mergeQuery(Function<NodeVariable[], Term<A>> getTerm) {
+		var name = partialFunction.name();
+		var abstractDomain = partialFunction.abstractDomain();
+		var type = abstractDomain.abstractType();
+		int arity = partialFunction.arity();
+		var parameters = new NodeVariable[arity];
+		var arguments = new ArrayList<Variable>(arity + 1);
+		for (int i = 0; i < arity; i++) {
+			var parameter = Variable.of("p" + i);
+			parameters[i] = parameter;
+			arguments.add(parameter);
+		}
+		var computedValue = Variable.of("computedValue", type);
+		arguments.add(computedValue);
+		var output = Variable.of("output", type);
+		return Query.builder(name)
+				.parameters(parameters)
+				.output(output)
+				.clause(
+						ModalConstraint.of(Modality.MAY, query.getDnf()).call(CallPolarity.POSITIVE, arguments),
+						output.assign(AbstractDomainTerms.meet(abstractDomain, computedValue,
+								getTerm.apply(parameters)))
+				)
+				.build();
 	}
 }
